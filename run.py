222f6290fcce36b700868b559e8f00739dffce13
cyy <1104256109@qq.com>
2025-11-10 22:10:26 +0800

    Default Changelist

--- run.py
+++ run.py
@@ -6,13 +6,29 @@ from conf.setting import REPORT_TYPE
 
 if __name__ == '__main__':
 
-    if REPORT_TYPE == 'allure':
-        pytest.main(
-            ['-s', '-v', '--alluredir=./report/temp', './testcase', '--clean-alluredir',
-             '--junitxml=./report/results.xml'])
+    # if REPORT_TYPE == 'allure':
+    #     pytest.main(
+    #         ['-s', '-v', '--alluredir=./report/temp', './testcase', '--clean-alluredir',
+    #          '--junitxml=./report/results.xml'])
+    #
+    #     shutil.copy('./environment.xml', './report/temp')
+    #     os.system(f'allure serve ./report/temp')
 
+    if REPORT_TYPE == 'allure':
+        pytest.main([
+            '-s', '-v',
+            '--alluredir=./report/temp',  # 生成 Allure 原始结果
+            './testcase',
+            '--clean-alluredir',
+            '--junitxml=./report/results.xml'
+        ])
+
+        # 复制环境配置文件（保持不变）
         shutil.copy('./environment.xml', './report/temp')
-        os.system(f'allure serve ./report/temp')
+
+        # 关键修改：用 allure generate 生成静态报告，替代 allure serve
+        # 生成到 ./report/allure-report 目录（与 Jenkins 配置的 Report path 对应）
+        os.system(f'allure generate ./report/temp -o ./report/allure-report --clean')
 
     elif REPORT_TYPE == 'tm':
         pytest.main(['-vs', '--pytest-tmreport-name=testReport.html', '--pytest-tmreport-path=./report/tmreport'])
